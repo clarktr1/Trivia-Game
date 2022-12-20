4d4916b86bc32cc1eddebaf4853b34266e5dc07f
Trey Clark <itreyclark@gmail.com>
2022-12-19 18:34:10 -0600

    first commit

---
+++ Assets/main.js
@@ -0,0 +1,149 @@
+// 1. Create HTML layout
+// 2. Set each button to toggle display for the subsequent question.
+// 3. Set a timer that counts down by 1 second from 30 seconds.
+// 4. On 'correct' answers, advance the question by changing display. On 'wrong answers', take away 5 seconds from the timer.
+// 4.a.(optional) In addition, the remaining timer time is added to the score. May also leave it at a static number.
+// 5. Final button displays high score screen and stores data into local Storage. Sorts in the OL.
+
+
+// Dom Elements
+intro = document.getElementById('intro');
+timeEl = document.getElementById('time');
+startEl = document.getElementById('start');
+startAgainEl = document.getElementById('start-again')
+highscoreEl = document.getElementById('high-score');
+scoreEl = document.getElementById('score');
+questionOne = document.getElementById('q1');
+questionTwo = document.getElementById('q2');
+questionThree = document.getElementById('q3');
+questionFour = document.getElementById('q4');
+questionFive = document.getElementById('q5');
+finishEl = document.getElementById('finish');
+listEl = document.getElementById('list');
+inputEl = document.getElementById('name');
+submitEl = document.getElementById('submit');
+wrongChoice = document.getElementById('wrong');
+
+// Timer
+
+startEl.addEventListener('click', setTime);
+
+let timerInterval;
+var secondsLeft = 30;
+
+function setTime() {
+
+    timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+  
+    }, 1000);
+  }
+
+function stopTimer (){
+    clearInterval(timerInterval);
+};
+
+function decrementTimer() {
+    secondsLeft -= 5;
+  }
+
+// Change Display
+
+highscoreEl.addEventListener('click', viewScore);
+startEl.addEventListener('click', toggleOne);
+document.getElementById('correct1').addEventListener('click', toggleTwo);
+document.getElementById('correct1').addEventListener('click', updateScore);
+document.getElementById('correct2').addEventListener('click', toggleThree);
+document.getElementById('correct2').addEventListener('click', updateScore);
+document.getElementById('correct3').addEventListener('click', toggleFour);
+document.getElementById('correct3').addEventListener('click', updateScore);
+document.getElementById('correct4').addEventListener('click', toggleFive);
+document.getElementById('correct4').addEventListener('click', updateScore);
+document.getElementById('correct5').addEventListener('click', finalToggle);
+document.getElementById('correct5').addEventListener('click', updateScore);
+document.getElementById('start-again').addEventListener('click', returnHome);
+document.getElementById('wrong1').addEventListener('click', decrementTimer);
+
+function updateScore() {
+    let currentScore = parseInt(scoreEl.textContent, 10);
+    currentScore += secondsLeft;
+    scoreEl.textContent = currentScore;
+};
+
+function viewScore() {
+    intro.style.display = 'none';
+    finishEl.style.display = 'block';
+};
+
+function toggleOne() {
+    intro.style.display = 'none';
+    questionOne.style.display = 'block';   
+    updateScore; 
+};
+
+function toggleTwo() {
+    questionOne.style.display = 'none';
+    questionTwo.style.display = 'block';  
+};
+
+function toggleThree() {
+    questionTwo.style.display = 'none';
+    questionThree.style.display = 'block';
+};
+
+function toggleFour() {
+    questionThree.style.display = 'none';
+    questionFour.style.display = 'block';
+};
+
+function toggleFive() {
+    questionFour.style.display = 'none';
+    questionFive.style.display = 'block';
+};
+
+function finalToggle(){
+    questionFive.style.display = 'none';
+    finishEl.style.display = 'block'
+    stopTimer();
+};
+
+function returnHome(){
+    finishEl.style.display = 'none'
+    intro.style.display = 'block'
+};
+
+
+//Set to Local Storage
+    submitEl = document.querySelector('#submit');
+    submitEl.addEventListener('submit', addToList);
+
+    function saveScore(){
+
+        var finalScore = scoreEl.textContent;
+        localStorage.setItem('score', finalScore);
+    }; 
+
+    function saveName(){
+        var inputValue = inputEl.value;
+        localStorage.setItem('name', inputValue);
+    };
+
+    var storedScore = localStorage.getItem('score')
+    var storedName = localStorage.getItem('name')
+
+    function addToList(event) {
+        event.preventDefault();
+        saveScore;
+        saveName;
+
+        var listItem = document.createElement('li');
+        listItem.textContent = storedName + storedScore;
+
+        listEl.appendchild(listItem);
+    };
+
